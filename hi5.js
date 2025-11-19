90a16f5b9dd3efe1c5ca661001f19c1e7c595428
three-1415 <sin.the.bean.three.1415@gmail.com>
2025-11-18 21:21:25 -0800

    Create hi5.js

---
+++ hi5.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/active/go/',
+    bare:'/b/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: 'https://raw.githubusercontent.com/three-1415/loader/refs/heads/main/hi9.js',
+    bundle: 'https://raw.githubusercontent.com/three-1415/loader/refs/heads/main/hi4.js',
+    config: 'https://raw.githubusercontent.com/three-1415/loader/refs/heads/main/hi5.js',
+    sw: 'https://raw.githubusercontent.com/three-1415/loader/refs/heads/main/hi8.js',
+};
