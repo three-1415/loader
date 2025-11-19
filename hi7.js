d089058697d43ba09fc1da073a3cc7ab5121f562
three-1415 <sin.the.bean.three.1415@gmail.com>
2025-11-18 21:30:19 -0800

    Delete hi7.js

--- hi7.js
+++
@@ -1,5 +0,0 @@
-importScripts("https://raw.githubusercontent.com/three-1415/loader/refs/heads/main/hi8.js");
-
-const sw = new UVServiceWorker();
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
